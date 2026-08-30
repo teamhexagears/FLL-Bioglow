d3dec67d864fcf5e9f98ce0252f2c89c1efc5bcf
teamhexagears <teamhexagears@gmail.com>
2026-08-30 14:05:04 -0700

    Add attachment reset to all missions

--- pybricks/mission2.py
+++ pybricks/mission2.py
@@ -1,5 +1,9 @@
 if __name__ == "__main__":
     import main
 
+from pybricks.tools import run_task
+
 def m2(robot):
+    run_task(robot.both_attachment_reset(500, 400))
+
     robot.move(100)
--- pybricks/mission5.py
+++ pybricks/mission5.py
@@ -1,6 +1,10 @@
 if __name__ == "__main__":
     import main
 
+from pybricks.tools import run_task
+
 def m5(robot):
+    run_task(robot.both_attachment_reset(500, 400))
+
     robot.right_attachment_turn(100)
     robot.right_attachment_turn(1000, 500)
