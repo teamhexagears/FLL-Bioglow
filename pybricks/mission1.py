43c11902cd81e56ef7c63a0943d4d47e7c9df8ca
Austin Lin <austinlin0520@gmail.com>
2026-09-11 18:04:18 -0700

    mission one

--- pybricks/mission1.py
+++ pybricks/mission1.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
 from pybricks.tools import run_task
 
 def m1(robot):
-    run_task(robot.both_attachment_reset(340))
-    run_task(robot.both_attachment_turn(-350, -350, 100, 100))
-    run_task(robot.both_attachment_turn(distance=-5, right_angle=5,left_angle=5))
-    #need to check
+    run_task(robot.both_attachment_reset(340, 100))
+    run_task(robot.both_attachment_turn(-347, -347, 100, 100))
+    run_task(robot.both_attachment_turn(distance=-58, right_angle=-60,left_angle=-60))
+    robot.
+    run_task(robot.both_attachment_turn(-349, -349, 100, 100))
