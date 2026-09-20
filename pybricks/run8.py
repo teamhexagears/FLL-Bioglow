aca2746f31da88f50c56ce0e7351a73181ffd9f2
Chloe Ho <chloehappyrun@gmail.com>
2026-09-20 15:11:01 -0700

    created/ update M7

--- pybricks/run8.py
+++ pybricks/run8.py
@@ -1,4 +1,4 @@
-# team Red - M3
+# team Blue - M7 & M12
 
 if __name__ == "__main__":
     import main
@@ -6,4 +6,11 @@ if __name__ == "__main__":
 from pybricks.tools import run_task
 
 def r8(robot):
-    run_task(robot.both_attachment_reset(34, 200))
+  robot.move(74) 
+  robot.turn(68) 
+  robot.move(8)
+  robot.right_attachment_turn(-120) 
+  robot.move(-8)
+  robot.left_attachment_turn(-100) 
+  robot.turn(-85)
+  robot.move(74)
