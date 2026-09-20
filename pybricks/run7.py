2e24c573350690b6fc9557fc6f3a7764f4b3f722
Chloe Ho <chloehappyrun@gmail.com>
2026-09-20 16:04:48 -0700

    Chloe fiX LEFT ATTACHMENT

--- pybricks/run7.py
+++ pybricks/run7.py
@@ -9,6 +9,7 @@ def r7(robot):
   robot.move(44)
   robot.turn(41)
   robot.move(17)
+  robot.left_attachment_turn(230)
   robot.right_attachment_turn(-5)
   robot.turn(65)
   robot.move(-20)
--- pybricks/run8.py
+++ pybricks/run8.py
@@ -10,7 +10,5 @@ def r8(robot):
   robot.turn(68) 
   robot.move(8)
   robot.right_attachment_turn(-120) 
-  robot.move(-8)
   robot.left_attachment_turn(-100) 
-  robot.turn(-85)
-  robot.move(74)
+
